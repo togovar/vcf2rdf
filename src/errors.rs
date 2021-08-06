a36b8de4aa8f9116c58ff12131c6af7591fa4238
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2021-08-06 19:01:23 +0900

    remove unused errors and fix typo

--- src/errors.rs
+++ src/errors.rs
@@ -17,41 +17,20 @@ pub enum Error {
     #[error(transparent)]
     SerdeYamlError(#[from] serde_yaml::Error),
 
-    #[error("argument error: {0}")]
-    ArgumentError(String),
-
-    #[error("file not found: {0}")]
+    #[error("File not found: {0}")]
     FileNotFoundError(String),
 
-    #[error("path contains invalid character: {0}")]
+    #[error("Path contains invalid character: {0}")]
     FilePathError(String),
 
-    #[error("index file not found: {0}")]
+    #[error("Index file not found: {0}")]
     IndexNotFoundError(String),
 
     #[error("Both reference and alternate must not be empty.")]
     InvalidRefAltError,
 
-    #[error("Chromosomes not found: {0}")]
-    NoChromosomeError(String),
-
-    #[error("Chromosomes cannot be uniquely determined: {0}")]
-    AmbiguousChromosomeError(String),
-
     #[error("Invalid reference ID.")]
     ReferenceIndexError,
-
-    #[error("Reference not found for index={0}")]
-    ReferenceSequenceNotFoundError(u32),
-
-    #[error("Reference name not found for {0}")]
-    ReferenceNameNotFoundError(String),
-
-    #[error("Info definition not found for {0}")]
-    InfoNotFoundError(String),
-
-    #[error("Unsupported assembly version: {0}")]
-    UnsupportedAssemblyError(String),
 
     #[error("The compression of {0} is not BGZF")]
     NotBgzipFileError(String),
@@ -64,9 +43,6 @@ pub enum Error {
 
     #[error("Could not write {0} bytes")]
     BgzipWriteError(usize),
-
-    #[error("Could not write index to {0}")]
-    IndexWriteError(String),
 
     #[error("tbx_index_build failed: {0}")]
     IndexBuildFailedError(String),
